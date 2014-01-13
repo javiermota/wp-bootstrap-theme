a0ad2e4f8d6eb767354ca59aeef83acfeb22a679
Javier Mota <javimota83@gmail.com>
2014-01-13 17:55:08 +0100

    Add bower and clean last bootstrap files

--- header.php
+++ header.php
@@ -4,20 +4,20 @@
     <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf8" />
-		
-		<link href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+		<link href="<?php bloginfo('template_directory'); ?>/css/main.css" rel="stylesheet" media="screen">
 		<link href="<?php bloginfo('stylesheet_url');?>" type="text/css" rel="stylesheet" media="screen, projection" />
 		<script src="http://code.jquery.com/jquery.js"></script>
 		<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
-		
-		
+
+
 		<?php wp_head(); ?>
-		
-    
+
+
   </head>
- 
+
   <body <?php body_class($class); ?>>
-    
+
     <nav class="navbar navbar-default" role="navigation">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
@@ -28,7 +28,7 @@
         </button>
         <a class="navbar-brand" href="<?php echo home_url(); ?>">Dev Site</a>
       </div>
-   
+
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse">
        <?php wp_nav_menu( array('menu' => 'Main', 'menu_class' => 'nav navbar-nav navbar-right', 'depth'=> 3, 'container'=> false, 'walker'=> new Bootstrap_Walker_Nav_Menu)); ?>
@@ -36,8 +36,8 @@
     </nav>
 
     <div id="main-container" class="container">
-    
-    
-    
-    	
-    	
+
+
+
+
+
