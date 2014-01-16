8416a78c3ac5d74d0d1e1256524eaf5f2b8fc9d6
javier <javier@debianJoomla.javimota.org>
2014-01-16 18:18:26 +0100

    Add footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+    </div>
+    <footer class="main-footer">
+      <div class="container">
+        <h2>Contact with us in social media</h2>
+        <div class="social-icons">
+          <div class="circle-icon">
+            <i class="fa fa-facebook fa-2x"></i>
+          </div>
+          <div class="circle-icon">
+            <i class="fa fa-twitter fa-2x"></i>
+          </div>
+          <div class="circle-icon">
+            <i class="fa fa-google-plus fa-2x"></i>
+          </div>
+          <div class="circle-icon">
+            <i class="fa fa-linkedin fa-2x"></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
