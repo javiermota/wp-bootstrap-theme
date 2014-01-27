fbda579fc897116f9a9f6fca0bab87689144d1ff
Javier Mota Diaz <javimota83@gmail.com>
2014-01-27 17:57:44 +0100

    Create functions.php from scratch

--- header.php
+++ header.php
@@ -2,24 +2,24 @@
 <html>
   <head>
     <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta charset="utf8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf8" />
 
     <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Mrs+Sheppards' rel='stylesheet' type='text/css'>
-    <link href="<?php bloginfo('template_directory'); ?>/css/main.css" rel="stylesheet" media="screen">
-		<link href="<?php bloginfo('stylesheet_url');?>" type="text/css" rel="stylesheet" media="screen, projection" />
-		<script src="http://code.jquery.com/jquery.js"></script>
-		<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+    <link href="<?php echo THEMEPATH; ?>/css/main.css" rel="stylesheet" media="screen">
+    <link href="<?php bloginfo( 'stylesheet_url' );?>" type="text/css" rel="stylesheet" media="screen, projection" />
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="<?php bloginfo( 'template_directory' ); ?>/js/bootstrap.min.js"></script>
 
 
-		<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
 
   </head>
 
-  <body <?php body_class($class); ?>>
+  <body>
 
     <header class="main-header">
       <div class="container">
@@ -31,15 +31,8 @@
             <i class="fa fa-bars fa-2x"></i>
         </button>
 
-        <nav  id="top-menu" class="top-menu collapse">
-          <?php wp_nav_menu( array('menu' => 'Main', 'menu_class' => '', 'depth'=> 3, 'container'=> false, 'walker'=> new Bootstrap_Walker_Nav_Menu)); ?>
-        </nav>
+        <?php wp_nav_menu( array( 'menu' => 'Main', 'menu_class' => '', 'container'=> 'nav', 'container_class'=> 'top-menu collapse' ) ); ?>
       </div>
     </header>
 
     <div id="main-container" class="container">
-
-
-
-
-
